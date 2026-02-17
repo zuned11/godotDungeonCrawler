809c35ee89b8508b40944e608a7197dac9daf402
zuned11 <alex.hashemi@me.com>
2026-02-16 20:41:46 -0500

    testing new area3d

--- scripts/WeaponComponent.gd
+++ scripts/WeaponComponent.gd
@@ -1,64 +1,53 @@
 class_name WeaponComponent 
 extends Node
 
-@onready var weapon: Area3D = $Hitbox
-
-
-
-#func _unhandled_input(event: InputEvent) -> void:
-	#if event.is_action_pressed("ui_accept") and Input.MOUSE_MODE_CAPTURED:
-		#weapon.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-
-extends Node3D
-
 @export_category("Weapon Stats")
-@export var damage: float = 25.0
+@export var attack_damage: float = 25.0
+@export var attack_range: float = 5.0 #not used yet
 
-@onready var anim = $AnimationPlayer
-@onready var swooshSound = $SwooshSound
+@export var weapon_area: Area3D
+@export var animation_player: AnimationPlayer
 
-var can_attack = false
-var enemies_in_range = []
+#@onready var swooshSound = $SwooshSound
 
+var attack_on_cooldown: bool = true
 
 func _ready() -> void:
-	anim.play("equip")
+	animation_player.play("equip")
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept") and Input.MOUSE_MODE_CAPTURED:
+		animation_player.play("attack")
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if !anim.is_playing():
-		can_attack = true
-	
-	print("processing weapon")
-	if Input.is_action_pressed("attack") and can_attack and not anim.is_playing():
+	if !animation_player.is_playing():
+		attack_on_cooldown = false
+		
+	if Input.is_action_pressed("attack") and not attack_on_cooldown:
 		print("attacking")
-		anim.play("attack")
-		swooshSound.play()
-		can_attack = false
+		animation_player.play("attack")
+		attack_on_cooldown = true
+		var enemies_in_range: Array[Node3D] = weapon_area.get_overlapping_bodies()
 		if !enemies_in_range.is_empty():
 			for e in enemies_in_range:
 				pass
 
-func _on_hitbox_body_entered(body: Node3D) -> void:
-	if body.is_in_group("enemy") and not enemies_in_range.has(body):
-		enemies_in_range.append(body)
-		
 
-func _on_hitbox_body_exited(body: Node3D) -> void:
-	if enemies_in_range.has(body):
-		enemies_in_range.erase(body)
-		
 
 func _on_anim_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "equip":
-		can_attack = true
-		visible = true
+		attack_on_cooldown = false
 	elif anim_name == "attack":
-		can_attack = true
-		anim.play("RESET")
+		attack_on_cooldown = false
+		animation_player.play("RESET")
+
+#
+#func _on_hitbox_body_entered(body: Node3D) -> void:
+	#if body.is_in_group("enemy") and not enemies_in_range.has(body):
+		#enemies_in_range.append(body)
+		#
+#
+#func _on_hitbox_body_exited(body: Node3D) -> void:
+	#if enemies_in_range.has(body):
+		#enemies_in_range.erase(body)
